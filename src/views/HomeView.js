3068247324862c19ebf4db3dd9880f324e3db8e9
Pham Xuan Duy <xduy.pham@gmail.com>
2020-10-01 01:37:21 +0700

    feat: react-native-permissions

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-	Text, TouchableOpacity, StyleSheet, Image, ImageBackground, Dimensions
+	Text, TouchableOpacity, StyleSheet, Image, ImageBackground, Dimensions, Platform
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+	request, check, PERMISSIONS, RESULTS
+} from 'react-native-permissions';
+
 import { colors } from '../constants/colors';
 
-const { width: WIDTH_SCREEN, height: HEIGHT_SCREEM } = Dimensions.get('screen');
+const { width: WIDTH_SCREEN, height: HEIGHT_SCREEN } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
 	container: {
 		width: WIDTH_SCREEN,
 		alignItems: 'center',
-		height: HEIGHT_SCREEM
+		height: HEIGHT_SCREEN
 	},
 	button: {
 		width: '90%',
@@ -49,8 +53,48 @@ class HomeView extends React.Component {
 		this.state = {};
 	}
 
-	render() {
+	checkGPS = (screenName) => {
 		const { navigation } = this.props;
+		check(
+			Platform.select({
+				android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+				ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+			})
+		).then((result) => {
+			switch (result) {
+				case RESULTS.GRANTED:
+					navigation.navigate(screenName);
+					break;
+				default:
+					this.requestGPS(screenName);
+					break;
+			}
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+
+	requestGPS = (screenName) => {
+		const { navigation } = this.props;
+		request(
+			Platform.select({
+				android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+				ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+			})
+		).then((result) => {
+			switch (result) {
+				case RESULTS.GRANTED:
+					navigation.navigate(screenName);
+					break;
+				default:
+					break;
+			}
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+
+	render() {
 		return (
 			<SafeAreaView style={styles.container}>
 				<ImageBackground
@@ -59,13 +103,13 @@ class HomeView extends React.Component {
 					resizeMode='cover'
 				>
 					<Image source={require('../assets/logo/enouvo.png')} style={styles.logo} />
-					<TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Search')}>
+					<TouchableOpacity style={styles.button} onPress={() => this.checkGPS('Search')}>
 						<Text style={styles.text}>Search Filters</Text>
 					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Maps')}>
+					<TouchableOpacity style={styles.button} onPress={() => this.checkGPS('Maps')}>
 						<Text style={styles.text}>Maps</Text>
 					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Applications')}>
+					<TouchableOpacity style={styles.button} onPress={() => this.checkGPS('Applications')}>
 						<Text style={styles.text}>Application</Text>
 					</TouchableOpacity>
 				</ImageBackground>
